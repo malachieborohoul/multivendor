b881dacdea7e688205fc868b500c72fe71db4e47
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-11 01:10:07 +0100

    Models ProductCategory

--- main/models.py
+++ main/models.py
@@ -6,3 +6,10 @@ class Vendor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.TextField(null=True)
 
+# ProductCategory
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return self.title
