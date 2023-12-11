63f8b6509c382be09443c6fcc3c6a229e5cca7d2
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-11 01:06:06 +0100

    Models Vendors

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 # ForeignKe
 from django.db import models
 from django.contrib.auth.models import User
+#Vendor models
 class Vendor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.TextField(null=True)
