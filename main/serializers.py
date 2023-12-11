5377c8d03939e876c33d7a1a4f8639ca662f508d
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-11 03:25:17 +0100

    Serializers depth

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from rest_framework.fields import empty
 from . import models
-class SellerSerializer(serializers.ModelSerializer):
+class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
         fields =['user', 'address']
+        
+    def __init__(self, *args, **kwargs):
+        super(VendorSerializer,self).__init__(*args, **kwargs)
+        self.Meta.depth=1
--- main/views.py
+++ main/views.py
@@ -6,10 +6,10 @@ from rest_framework import generics, permissions
 
 class VendorList(generics.ListAPIView):
     queryset = models.Vendor.objects.all()
-    serializer_class = serializers.SellerSerializer
+    serializer_class = serializers.VendorSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class VendorDetail(generics.RetrieveAPIView):
     queryset = models.Vendor.objects.all()
-    serializer_class = serializers.SellerSerializer
+    serializer_class = serializers.VendorSerializer
     permission_classes = [permissions.IsAuthenticated]
