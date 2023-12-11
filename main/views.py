ba945f4d8315eff0bbeec4a91d11589509cc6ef6
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-11 03:49:18 +0100

    Add foreign key in Product

--- main/views.py
+++ main/views.py
@@ -4,12 +4,15 @@ from . import models
 from rest_framework import generics, permissions
 # Create your views here.
 
-class VendorList(generics.ListAPIView):
+class VendorList(generics.ListCreateAPIView):
     queryset = models.Vendor.objects.all()
     serializer_class = serializers.VendorSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class VendorDetail(generics.RetrieveAPIView):
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
+
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Vendor.objects.all()
     serializer_class = serializers.VendorSerializer
     permission_classes = [permissions.IsAuthenticated]
