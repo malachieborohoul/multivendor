03a9e2f81e3396b3fc0d37748f2106232ae7e3a4
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-11 02:15:29 +0100

    Add permissions

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from . import serializers
 from . import models
-from rest_framework import generics
+from rest_framework import generics, permissions
 # Create your views here.
 
 class VendorList(generics.ListAPIView):
     queryset = models.Vendor.objects.all()
     serializer_class = serializers.SellerSerializer
+    permission_classes = [permissions.IsAuthenticated]
