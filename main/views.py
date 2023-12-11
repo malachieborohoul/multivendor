30a9a20160c22a4756fd40d660d5a369dd36fcec
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-11 01:39:58 +0100

    VendorList

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from . import serializers
+from . import models
+from rest_framework import generics
 # Create your views here.
 
+class VendorList(generics.ListAPIView):
+    serializer_class = serializers.SellerSerializer
+    queryset = models.Vendor.objects.all()
