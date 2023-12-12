ca146a88bd4963a4154553abae5c78742fd75438
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-12 23:13:42 +0100

    Product Serializer

--- main/models.py
+++ main/models.py
@@ -17,7 +17,7 @@ class ProductCategory(models.Model):
     def __str__(self) -> str:
         return self.title
     
-# ProductProductProductProductProductProd
+# ProductProductProductProductProductPro
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
