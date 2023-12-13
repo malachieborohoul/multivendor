071dec66bf685e74a37f5e1f64f3fb25b584c51f
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-13 23:45:26 +0100

    Order & OrderItem views

--- main/models.py
+++ main/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.title
     
-# Customer ModelModeModelModeM
+# Customer ModelModeModelMode
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mobile = models.PositiveBigIntegerField()
