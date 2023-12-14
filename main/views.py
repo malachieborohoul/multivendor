8d05eb5d358984737307631e100930529d201db3
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-14 15:29:14 +0100

    Pagination views

--- main/views.py
+++ main/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from . import serializers
 from . import models
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, pagination
 # Create your views here.
 
 class VendorList(generics.ListCreateAPIView):
     queryset = models.Vendor.objects.all()
     serializer_class = serializers.VendorSerializer
     # permission_classes = [permissions.IsAuthenticated]
+    
 
     def perform_create(self, serializer):
         return super().perform_create(serializer)
@@ -19,7 +20,8 @@ class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class ProductList(generics.ListCreateAPIView):
     queryset = models.Product.objects.all()
-    serializer_class = serializers.ProductSerializer
+    serializer_class = serializers.ProductSerializer,
+    pagination_class= pagination.LimitOffsetPagination 
 
     def perform_create(self, serializer):
         return super().perform_create(serializer)
