f99d88b6889f8da43bc3c2b0bf24d4f0f786078c
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-18 22:24:39 +0100

    ProductRating Serializer

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework import routers
-# DefaultRouteDefaultRouteDefa
+# DefaultRouteDefaultRouteDef
 router = routers.DefaultRouter()
 router.register('address', views.CustomerAddressViewSet)
 router.register('productrating', views.ProductRatingViewSet)
