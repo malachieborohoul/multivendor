742bee518672eb718aa17c972c89da271f8cfa87
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-29 16:20:15 +0100

    Update urls

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 }
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 
-MEDIA_URL='/media'
+MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 # CORS_ALLOWED_ORIGINS=[
 #     'http://127.0.0.1:3000',
--- backend_api/urls.py
+++ backend_api/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')) ,
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+ 
